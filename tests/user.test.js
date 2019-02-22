fc10c02e78339e76ade4d3e24cb4437850a90420
C S Kshyatisekhar Panda <31128375+kshyati@users.noreply.github.com>
2019-02-22 19:47:16 +0530

    Update user.test.js

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,44 +1,62 @@
 import expect from 'expect'
-import request  from 'supertest';
-import {ObjectID}  from 'mongodb';
+import request from 'supertest';
+import {
+  ObjectID
+} from 'mongodb';
 
-import {app} from '../server';
-import {userModel} from './../models/user.model';
+import {
+  app
+} from '../server';
+import {
+  userModel
+} from './../models/user.model';
 
 // Test Case created for User Register only using mochaJS and Super Test
 
 const userData = {
   "name": "HighIam!1csc1",
-  "email": "cs@cs.cs",
+  "email": "cs@cssdssvddf.cs",
   "password": "55555555",
   "address": "India",
   "phone": "7749896223"
 };
-describe('POST /User Data', () => {
+describe('POST /user/userRegister', () => {
   it('should create a new user', (done) => {
-    let text = 'I am here';
     request(app)
       .post('/user/userRegister')
-      .send({
+      .send(
         userData
-      })
+      )
       .expect(200)
       .expect((res) => {
         // console.log(res);
-        expect(res.body.data.text).toBe(text);
+        expect(res.body.data).toBe(
+          'You are successfully registered');
       })
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-
-        userModel.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
-          done();
-        }).catch((e) => done(e));
+        done();
+      });
+  });
+  it('should have proper data for register', (done) => {
+    request(app)
+      .post('/user/userRegister')
+      .send({
+        "Ok": "fdsfdsf"
+      })
+      .expect(404)
+      .expect((res) => {
+        // console.log(res);
+        expect('').toBe(
+          '');
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        done();
       });
   });
 })
-
-
