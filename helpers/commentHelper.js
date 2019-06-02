ef938e2355d6ef59dbb67ecc5d418c50f19bb897
C S Kshyatisekhar Panda <31128375+kshyati@users.noreply.github.com>
2019-06-02 18:48:08 +0530

    Modified Like/Dislike logic

--- helpers/commentHelper.js
+++ helpers/commentHelper.js
@@ -9,6 +9,10 @@ import {
   commentModel
 } from '../models/comment.model';
 
+import {
+  likeDislikeModel
+} from '../models/likeDislike.model';
+
 
 /**
  * @desc Add the new comments on the individual bloggs
@@ -129,53 +133,106 @@ const deleteComment = (req, res) => {
 const addLikeDislike = (req, res) => {
   return new Promise((resolve, reject) => {
     try {
-      let id = req.query.id;
-      var body = _.pick(req.body, ['isLike','totalLikeCount','totalDislikeCount']);
-      if (!ObjectID.isValid(id)) {
+      let commentId = req.query.id;
+      const { isLike, blogId, userId } = req.body;
+      var body = _.pick(req.body, ['isLike', 'totalLikeCount', 'totalDislikeCount']);
+      if (!ObjectID.isValid(commentId)) {
         return resolve({
           status: HttpStatusCode.INTERNAL_SERVER_ERROR,
           data: 'Invalid Object Id'
         });
       }
-      commentModel.findById(id).then((doc) => {
-        if (doc) {
-          if (body.isLike== true){
-            body.totalLikeCount= doc.totalLikeCount + 1;
-          }
-          else {
-            body.totalDislikeCount=(doc.totalDislikeCount)+1;
-          }
-          commentModel.findByIdAndUpdate(id, { $set: body}, { new: false }).then((docs) => {
-            if (!docs) {
-              return reject({
-                status: HttpStatusCode.NOT_FOUND,
-                data: 'Not Found'
-              });
-            }
-            return resolve({
-                  status: HttpStatusCode.OK,
-                  data: `Your like/dislike has been updated!`
-                });
+      console.log(isLike, blogId, userId, commentId);
+      let likeDislikeData = new likeDislikeModel({
+        commentId: commentId,
+        blogId: blogId,
+        userId: userId,
+        isLike: isLike
+      });
+      likeDislikeModel.find({
+        'commentId': commentId,
+        'blogId': blogId,
+        'userId': userId
+      }, (err, docs) => {
 
+        var isValid = docs;
+        if (isValid.length === 0) {
+          likeDislikeData.save().then((res) => {
+            if (res.length === 1) {
+              commentModel.findById(id).then((doc) => {
+                if (doc) {
+                  if (doc.isLike == true) {
+                    doc.totalLikeCount = doc.totalLikeCount + 1;
+                  }
+                  else {
+                    doc.totalLikeCount = (doc.totalLikeCount) - 1;
+                  }
+                  commentModel.findByIdAndUpdate(id, { $set: doc }, { new: false }).then((totalUpdated) => {
+                    if (!totalUpdated) {
+                      return reject({
+                        status: HttpStatusCode.NOT_FOUND,
+                        data: 'Not Found'
+                      });
+                    }
+                    return resolve({
+                      status: HttpStatusCode.OK,
+                      data: `Your like/dislike has been updated!`
+                    });
+
+                  }).catch((e) => {
+                    return reject({
+                      status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+                      data: e.message
+                    });
+                  })
+                }
+                else {
+                  return resolve({
+                    status: HttpStatusCode.OK,
+                    data: 'Data is not available as expected!'
+                  });
+                }
+              }, (e) => {
+                return reject({
+                  status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+                  data: e.message
+                });
+              });
+
+            }
+            console.log(res);
           }).catch((e) => {
             return reject({
               status: HttpStatusCode.INTERNAL_SERVER_ERROR,
               data: e.message
             });
           })
+        } else {
+          if (isValid.length === 1) {
+            console.log(JSON.stringify(isValid[0]._id));
+            likeDislikeModel.findByIdAndUpdate(isValid[0]._id, { $set: { 'isLike': isLike } }, { new: false }).then((newTotalUpdated) => {
+              console.log(newTotalUpdated);
+              if (!newTotalUpdated) {
+                return reject({
+                  status: HttpStatusCode.NOT_FOUND,
+                  data: 'Not Found'
+                });
+              }
+              return resolve({
+                status: HttpStatusCode.OK,
+                data: 'Your like/dislike has been updated!'
+              });
+            })
+          }
+
+          // return resolve({
+          //   status: HttpStatusCode.NOT_FOUND,
+          //   data: 'Invalid Request'
+          // });
         }
-        else {
-          return resolve({
-            status: HttpStatusCode.OK,
-            data: 'Data is not available as expected!'
-          });
-        }
-      }, (e) => {
-        return reject({
-          status: HttpStatusCode.INTERNAL_SERVER_ERROR,
-          data: e.message
-        });
-      });
+      })
+
+
     } catch (e) {
       return reject({
         status: HttpStatusCode.INTERNAL_SERVER_ERROR,
